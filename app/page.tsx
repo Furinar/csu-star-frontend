7cb528f54141923ec843c15fdcddb189c8232760
Furina <3559551198@qq.com>
2026-02-25 22:52:23 +0800

    feat: 重构导航栏组件，更新路径和添加导航项

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,19 @@
-import Header from "@/components/welcome/Header";
+import Header from "@/components/Header";
 import "./globals.css";
 
 export default function Welcome() {
   return (
     <>
       {/* 导航栏 */}
-      <Header />
+      <Header
+        navItems={[
+          { label: "首页", href: "#home", icon: "uil-estate" },
+          { label: "关于", href: "#about", icon: "uil-user" },
+          { label: "展示", href: "#portfolio", icon: "uil-scenery" },
+          { label: "技术栈", href: "#skills", icon: "uil-file-alt" },
+          { label: "联系", href: "#contact", icon: "uil-message" },
+        ]}
+      />
 
       {/* 主要内容 */}
       <main></main>
